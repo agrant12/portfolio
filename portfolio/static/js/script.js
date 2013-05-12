d19dfada3b0232fbdeb89597272991e5b6981af3
Alvin Grant <alving.nyc@gmail.com>
2013-05-12 12:29:24 -0400

    Added hover function for portfolio page

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,11 +1,33 @@
-var userAgent = Grant.utils.userAgent();
+jQuery(document).ready(function($){
 
-if(!userAgent){
-	console.log("Hey there");
-}
+	/* Set Varibales for Featured Works*/
 
-var $work = $('#work');
+	var $work = $('#work');
+	var $front = $('.front');
+	var $back = $('.back');
+	var defaultToggleState = true;
 
+	/* Toggle Back on Hover & Set Opacity of Front Div */
+	
+	$work.on('hover', function(){
+		if (defaultToggleState){
+			$back.slideDown('fast');
+
+			defaultToggleState = false;
+		} else{
+			$back.slideUp('fast');
+
+			defaultToggleState = true;
+		}
+	});
+
+	
+
+	/*$work.hover(function(){
+		$back.toggle('fast');
+		$
+	});*/
+});
 
 
 
