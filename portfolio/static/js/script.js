ec9173d2be4baa1779f80d23e8f3e99024eb6c4e
Alvin Grant <alving.nyc@gmail.com>
2013-05-12 20:06:44 -0400

    Refactored work hover function

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -2,31 +2,26 @@ jQuery(document).ready(function($){
 
 	/* Set Varibales for Featured Works*/
 
-	var $work = $('#work');
+	var $work = $('.work');
 	var $front = $('.front');
 	var $back = $('.back');
 	var defaultToggleState = true;
 
 	/* Toggle Back on Hover & Set Opacity of Front Div */
-	
+
 	$work.on('hover', function(){
 		if (defaultToggleState){
-			$back.slideDown('fast');
+			
+			$(this).find($back).slideDown('fast');
 
 			defaultToggleState = false;
 		} else{
-			$back.slideUp('fast');
-
+			
+			$(this).find($back).slideUp('fast');
+			
 			defaultToggleState = true;
 		}
 	});
-
-	
-
-	/*$work.hover(function(){
-		$back.toggle('fast');
-		$
-	});*/
 });
 
 
