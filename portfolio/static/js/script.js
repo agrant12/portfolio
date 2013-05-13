92a62567ad29b55d1a10cd3f55bbf8caf3694996
Alvin Grant <alving.nyc@gmail.com>
2013-05-13 10:48:12 -0400

    Add content to work page
    Add new styles for html page headers

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
 	var $back = $('.back');
 	var defaultToggleState = true;
 
-	/* Toggle Back on Hover & Set Opacity of Front Div */
+	/* Toggle Back on Hover */
 
 	$work.on('hover', function(){
 		if (defaultToggleState){
